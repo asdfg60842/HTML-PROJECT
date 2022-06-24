71555a4fe2b95f10ccb0d4f3a95b05a1e1c15ee4
CAZ <asdfg60842@gmail.com>
2022-06-24 23:52:32 +0900

    Delete references directory

--- references/function/test/test.js
+++
@@ -1,9 +0,0 @@
-var loginBox = document.querySelector('.outBox');
-var loginInputBox = document.querySelector('#sampleId');
-loginInputBox.addEventListener('keyup', function(){
-    if(!loginInputBox.value == ''){
-        loginBox.classList.add('existence');   
-    }else{
-        loginBox.classList.remove('existence');   
-    }
-});
